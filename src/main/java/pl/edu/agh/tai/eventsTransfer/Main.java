35ae6c458854ea8333805a5082ae3cfb46e6d1f9
Piotr Grabiec <pgrabiec7@gmail.com>
2017-05-21 12:51:16 +0200

    Created project structure

---
+++ src/main/java/pl/edu/agh/tai/eventsTransfer/Main.java
@@ -0,0 +1,4 @@
+package pl.edu.agh.tai.eventsTransfer;
+
+public class Main {
+}
---
+++ src/test/java/pl/edu/agh/tai/eventsTransfer/MainTest.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.tai.eventsTransfer;
+
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void testMain() throws Exception {
+
+    }
+}
